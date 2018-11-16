88f1fb52ceda488045e0c1266e9e6cd7bcb3378e
radushi <radushi@gmail.com>
2018-11-16 10:30:41 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+# # if 3>2:
+#     print("It works.")
+
+def hi(name):
+    print("Hi " + name + "!")
+
+girls = ["Rachel", "Monica", "Phoebe","Ola", "Radka"]
+
+for name in girls:
+    hi(name)
+    print("Next girl")
